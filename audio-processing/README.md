5c7c70b86024118f0ee2ccd15d7280f6cc935017
Alex Shank <ashank31@gmail.com>
2022-01-04 17:02:08 -0600

    Cleaning up unused programs/scripts.

--- audio-processing/README.md
+++
@@ -1,2 +0,0 @@
-## Audio Processing to Find BPM
-Sample audio files found [here](https://docs.espressif.com/projects/esp-adf/en/latest/design-guide/audio-samples.html).
--- financial-analysis/constants.py
+++
@@ -1,7 +0,0 @@
-# all possible account types
-ACCOUNTS = ["Discover 9225", "Virtual Wallet", "Cash", "Venmo", "Mom's International Card"]
-
-# main catagories and their sub-catagories 
-CATAGORIES = {"Travel": ("Gas", "Uber"),
-    "Food": ("Beer", "Wine"),
-    "Fun": ("Movie Tickets", "Weed")}
